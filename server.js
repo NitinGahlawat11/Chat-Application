fccb9b62bc996445e058ec621f589574eec0cbb7
NitinGahlawat11 <webnitin640@gmail.com>
2018-08-07 20:37:22 +0530

    typing notification added

--- server.js
+++ server.js
@@ -1,42 +1,28 @@
 var express = require('express');
-var app = express();
+var socket= require('socket.io');
+var app=express();
+app.use(express.static('public'));
 
-var http = require('http');
-var server = http.createServer(app);
 
-var socket = require('socket.io');
 
-var io = socket(server);
-var msgs = [];
-var connected_users={};
+
+var server = app.listen(4000,function () {
+    console.log("server is listening on port 4000");
+
+})
+var io = socket(server);// socket will be listening on the server we have defined below
+
+// io will be looking for a connection method and each  new user will have its own socket,
+
 io.on('connection', function(socket){
-    console.log('connection established.');
-    socket.on('receive_message', function(data) {
 
-        msgs.push(data);
-
-        io.emit('get', data, connected_users);
-    });
-    socket.emit('all', msgs, connected_users);
-
-    socket.on('username', function(data){
-        console.log(data, socket.id);
-        connected_users[socket.id]=data;
-        console.log('connected_users= '+connected_users);
-    });
-    socket.on('disconnect', function(){
-        delete  connected_users[socket.id];
-        console.log(connected_users);
-        console.log('disconnected');
-    });
+    console.log('made socket connection',socket.id) ; // each socket will have its own unique id and we will get different id everytime we restart
 
 
+    socket.on('chat',function(data){
+        io.emit('chat',data);
+    })
+    socket.on('typing',function(data){
+        socket.broadcast.emit('typing',data);
+    })
 });
-
-
-app.use('/', express.static('public'));
-server.listen(process.env.PORT || 3000, function(){
-    console.log("server is listening on port 5000");
-});
-
-
